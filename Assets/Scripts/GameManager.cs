71ef9e121e2d1dad1e0e57a2d927f7eb8b15501e
semimarufbi <152785706+semimarufbi@users.noreply.github.com>
2024-10-15 01:40:24 -0300

    metodoDefinirdiametro

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,16 +13,20 @@ public class GameManager : MonoBehaviour
 
     // Start is called before the first frame update
    
-    public void GerarGrade()
+    public void GerarGrade() // gera a grade inicial do jogo
     {
         grade = new int[diametroDoCampo,diametroDoCampo];
         CameraSeguidora();
     }
     public void CameraSeguidora()
     {
-
+        // Centraliza a câmera no campo de jogo
         mainCamera.transform.position = new Vector3(diametroDoCampo / 2f - 0.5f, diametroDoCampo / 2f - 0.5f, -10);
         mainCamera.orthographicSize = 2f;
+    }
+    public void DefinirDiametro(string value)
+    {
+        diametroDoCampo = int.Parse(value);
     }
 
    
