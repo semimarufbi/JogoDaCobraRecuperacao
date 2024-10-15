ddf1083d8355283123dec23b3efccdff98eb4efb
semimarufbi <152785706+semimarufbi@users.noreply.github.com>
2024-10-15 01:00:39 -0300

    terminadometodostart

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public TextMeshProUGUI textoPontuacao; // referencia ao componente TextMeshPro
+    private int pontuacao; // Variavel para armazenar a pontuação
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
